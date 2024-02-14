5992ed11d0661d2fc6fb450eb48bb4210c8dd5ee
danyow-cheung <76671016+danyow-cheung@users.noreply.github.com>
2024-02-14 16:14:19 +0800

    0214

--- chapter3/runnable_proj/target/debug/deps/runnable_proj-905d80b860d8635d.d
+++
@@ -1,5 +0,0 @@
-/Users/danyow/Desktop/self-learning/code/chapter3/runnable_proj/target/debug/deps/runnable_proj-905d80b860d8635d: src/main.rs
-
-/Users/danyow/Desktop/self-learning/code/chapter3/runnable_proj/target/debug/deps/runnable_proj-905d80b860d8635d.d: src/main.rs
-
-src/main.rs:
--- chapter3/runnable_proj/target/debug/runnable_proj.d
+++
@@ -1,1 +0,0 @@
-/Users/danyow/Desktop/self-learning/code/chapter3/runnable_proj/target/debug/runnable_proj: /Users/danyow/Desktop/self-learning/code/chapter3/runnable_proj/src/main.rs
